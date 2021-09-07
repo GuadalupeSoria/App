efac895be17d033f05173e965a47a5681a4aa0f3
Guadalupe Soria <soriaguada01@gmail.com>
2021-09-07 17:04:56 -0300

    agregado de NavBar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar'
 
-function App() {
-   console.log('Hola, esta es mi app!')
 
+function App () {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />        
+        <NavBar/>
+        <img src={logo} className="App-logo" alt="logo" />       
       </header>
     </div>
   );
---
+++ src/components/NavBar.js
@@ -0,0 +1,18 @@
+const NavBar = () => {
+  return (
+  <div>
+     <h1>Sapphira</h1>
+     <img src=""></img>
+     <nav>
+			<ul>
+				<li><a href="#">Historia</a></li>
+				<li><a href="#">Productos</a></li>
+				<li><a href="#">¿Como se usa?</a></li>
+				<li><a href="#">Contacto</a></li>
+			</ul>
+		</nav>
+  </div>
+  )
+}
+  
+export default NavBar
