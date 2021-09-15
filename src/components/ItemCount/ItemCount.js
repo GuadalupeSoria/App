79c45dd84dd5ca796747100ddb17fe344a6e5085
Guadalupe Soria <soriaguada01@gmail.com>
2021-09-15 13:16:24 -0300

    correccion

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -1,20 +1,27 @@
 import { useState } from 'react'
 
 const Counter = () => {
-    const [count, setCount] = useState(0)
-       const funcionRestar = () => {
+     const [count, setCount] = useState(0)
+       
+     const funcionRestar = () => {
            if (count <= 0) {
-               console.log('no hago nada')
            } else {
                setCount(count - 1)     
            }
        }
 
+       const funcionStock = () => {
+        if (count >= 25) {
+        } else {
+            setCount(count + 1)     
+        }
+    }
+
     return (
         <div>
             <h1 style={{ color: '#B36A5E'}}>{count}</h1>
             <button onClick={funcionRestar}>-</button>
-            <button onClick={() => setCount(count + 1)}>+</button>
+            <button onClick={funcionStock}>+</button>
         </div>
     )
 }
