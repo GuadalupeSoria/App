5abb23d9edadc4b8c4855ee5bd0108159a55acf6
Guadalupe Soria <soriaguada01@gmail.com>
2021-09-15 19:46:30 -0300

    promises/map

--- src/App.js
+++ src/App.js
@@ -2,15 +2,40 @@ import './App.css';
 import NavBar from './components/NavBar/NavBar.js'
 import ItemListContainer from './components/Item/ItemListContainer';
 import Counter from './components/ItemCount/ItemCount.js'
+import { useState,useEffect } from 'react';
 
-function App () {
- 
-  return (
+
+const Productos = [
+  {id:'01', name:'Gua sha', description:'material:jade', stock:'10',price:1600,},
+  {id:'02', name:'Roller', description:'material:jade', stock:'12',price:1750},
+  {id:'03', name:'Mushroom', description:'material:jade', stock:'15',price:1400}]
+
+function getList () {
+   
+  return new Promise ((resolve, reject) => {
+    setTimeout(() => resolve (Productos),2000)
+  })
+}
+  const App = () => {
+    const [ListProducts,setListProducts] = useState([])
+
+    useEffect(() => {
+       const list = getList()
+
+       list.then(list => {
+         setListProducts(list)
+       })
+    }, [])
+  
+    return (
     <div className="App">
       <header className="App-header">
         <NavBar/>
         <ItemListContainer/>
       </header>
+      <ul>
+         { ListProducts.map(product => <li key={product.id}>{product.name}</li>)}
+        </ul>
       <Counter inicial={1} max={25}/>      
     </div>
   );
--- src/components/Item/ItemListContainer.js
+++ src/components/Item/ItemListContainer.js
@@ -1,7 +1,10 @@
+
 const ItemListContainer = () => {
     return (
         <h1>CATALOGO</h1>
     )
 }
 
+
+    
 export default ItemListContainer 
